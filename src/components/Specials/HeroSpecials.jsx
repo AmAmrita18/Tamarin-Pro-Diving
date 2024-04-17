f48d380d295922d28741cab7149f99170b671ced
Amrita Gupta <amamrita0818@gmail.com>
2024-04-17 16:04:58 +0530

    update headings for hero sections

--- src/components/Specials/HeroSpecials.jsx
+++ src/components/Specials/HeroSpecials.jsx
@@ -14,8 +14,8 @@ const HeroSpecials = () => {
     }}>
         <Header/>
         <div className='w-[90%] max-w-[1200px] mx-auto py-44'>
-            <h1 className="font-[700] text-center text-[70px] text-white leading-[85.15px]">Diving Courses</h1>
-            <p className="font-[400] text-center leading-[19.94px] text-[15px] text-white mt-12">Lorem ipsum dolor sit amet consectetur. Urna nibh sem risus nec interdum habitant. Porttitor augue enim turpis maecenas et adipiscing. Dis ipsum vitae ultrices vulputate sem quis dui eu. Amet tincidunt est elementum fermentum tellus velit. Et dolor risus tempor dui velit. Eget aenean placerat ac feugiat amet ac risus mus. </p>
+            <h1 className="font-[700] text-center text-[70px] text-white leading-[85.15px]">Packages</h1>
+            <p className="font-[400] text-center leading-[19.94px] text-[15px] text-white mt-12 w-[70%] mx-auto">Lorem ipsum dolor sit amet consectetur. Urna nibh sem risus nec interdum habitant. Porttitor augue enim turpis maecenas et adipiscing. Dis ipsum vitae ultrices vulputate sem quis dui eu. Amet tincidunt est elementum fermentum tellus velit. Et dolor risus tempor dui velit. Eget aenean placerat ac feugiat amet ac risus mus. </p>
         </div>
     </div>
   )
